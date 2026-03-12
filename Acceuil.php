3a79674dd176fab1ec9635c4917861a9f5c0f5b2
BentekhiciFarah <bentekhici.farah@gmail.com>
2026-03-12 11:20:34 +0100

    Extention php

--- Acceuil.php
+++ Acceuil.php
@@ -38,12 +38,11 @@
 
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
-	          <li class="nav-item"><a href="rooms.html" class="nav-link">Rooms</a></li>
-	          <li class="nav-item"><a href="restaurant.html" class="nav-link">Restaurant</a></li>
-	          <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-	          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+	          <li class="nav-item active"><a href="Acceuil.php" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="hebergement.php" class="nav-link">Rooms</a></li>
+	          <li class="nav-item"><a href="restaurant.php" class="nav-link">Restaurant</a></li>
+	          <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
 	        </ul>
 	      </div>
 	    </div>
